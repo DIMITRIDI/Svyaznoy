7e467d4797e8c235ff43ea103e194403da10e7a5
Dmitry <d.a16@yandex.ru>
2022-09-14 12:29:01 +0300

    Создание основных страниц и компонентов приложения, настройка маршрутизации.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Contact from "./pages/contact/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <>
+         <BrowserRouter>
+            <Header />
+            <Routes>
+               <Route path="/" element={ <Home /> } />
+               <Route path="/contact" element={ <Contact /> } />
+            </Routes>
+            <Footer />
+         </BrowserRouter>
+      </>
+   );
 }
 
 export default App;
